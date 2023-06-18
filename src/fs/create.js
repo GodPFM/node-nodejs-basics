2685b1d52ba7e5bc4fc9c6583016ed93f0a87377
Daniil Asmeev <asmeev.daniil@yandex.ru>
2023-06-18 09:11:00 +0500

    feat: complete create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
+import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const pathToFolder = path.resolve(__dirname, 'files')
+const pathToFile = path.resolve(pathToFolder, 'fresh.txt')
+
 const create = async () => {
-    // Write your code here 
+  fs.writeFile(pathToFile, 'I am fresh and young',{ flag: 'wx' } ,(err) => {
+    if (err) {
+      throw 'FS operation failed'
+    }
+  })
 };
 
 await create();
