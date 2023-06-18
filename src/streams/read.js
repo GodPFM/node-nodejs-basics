17de12d7df999db430b530b32e2d7442562edfb7
Daniil Asmeev <asmeev.daniil@yandex.ru>
2023-06-18 17:15:35 +0500

    feat: tasks streams complete

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,13 @@
-const read = async () => {
-    // Write your code here 
+import path from "path";
+import fs from "fs";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const read = async (pathToFile) => {
+  const readStream = fs.createReadStream(pathToFile);
+  readStream.pipe(process.stdout);
 };
 
-await read();
+await read(path.resolve(__dirname, "files", "fileToRead.txt"));
