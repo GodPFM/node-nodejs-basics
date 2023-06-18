b7ef2578808220b314d219cfbd715d527df708ee
Daniil Asmeev <asmeev.daniil@yandex.ru>
2023-06-18 13:52:52 +0500

    feat: task list.js complete

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,25 @@
-const list = async () => {
-    // Write your code here 
+import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const list = async (folder) => {
+  const pathToFolder = path.resolve(__dirname, folder);
+  fs.readdir(pathToFolder, {withFileTypes: true}, (err, files) => {
+    if (err) {
+      throw new Error('FS operation failed')
+    }
+    if (files.length === 0) {
+      throw new Error('There\'s no files')
+    }
+    for (const file of files) {
+      if (file.isFile()) {
+        process.stdout.write(`Find file - ${file.name}\n`)
+      }
+    }
+  })
 };
 
-await list();
+await list('files');
