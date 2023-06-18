3f1336b99908ea27b23c6c9f25af9445ea7a4229
Daniil Asmeev <asmeev.daniil@yandex.ru>
2023-06-18 13:30:08 +0500

    feat: task rename complete

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,31 @@
-const rename = async () => {
-    // Write your code here 
+import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+const rename = async (fileToRename, newName, folder) => {
+  const folderPath = path.resolve(__dirname, folder);
+  fs.readdir(folderPath, {withFileTypes: true}, (err, files) => {
+    if (err) {
+      throw new Error(err.message)
+    }
+    const filteredFilesByName = files.map((item) => {
+      if (item.name === fileToRename || item.name === newName) {
+        return item.name;
+      }
+    })
+    if (filteredFilesByName.includes(newName) || !filteredFilesByName.includes(fileToRename)) {
+      throw new Error('FS operation failed')
+    }
+    fs.rename(path.resolve(folderPath, fileToRename), path.resolve(folderPath, newName), (err) => {
+      if (err) {
+        throw new Error(err.message)
+      }
+    })
+  });
 };
 
-await rename();
+await rename('wrongFilename.txt', 'properFilename.md', 'files');
