a63bcbc510d4fadb85fe3798d93b1cf22574456a
Daniil Asmeev <asmeev.daniil@yandex.ru>
2023-06-18 14:53:23 +0500

    feat: task args.js complete

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,17 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv;
+  let result = '';
+  args.forEach((item, index) => {
+    if (item.startsWith('--')) {
+      if (args[index + 1] !== undefined && args[index + 1] !== null) {
+        if (result) {
+          result += ', '
+        }
+        result += `${item.replace('--', '')} is ${args[index + 1]}`;
+      }
+    }
+  });
+  process.stdout.write(result)
 };
 
 parseArgs();
